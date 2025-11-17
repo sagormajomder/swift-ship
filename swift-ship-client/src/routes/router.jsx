afa7e5408a636e6ed22714a8d236b30b712b45ac
Sagor Majomder <sagormajomder@gmail.com>
2025-11-17 17:47:16 +0600

    Wrap Login and Register pages with PrivateRoute for authentication protection

--- swift-ship-client/src/routes/router.jsx
+++ swift-ship-client/src/routes/router.jsx
@@ -3,6 +3,7 @@ import AuthLayout from '../layouts/AuthLayout';
 import RootLayout from '../layouts/RootLayout';
 import LoginPage from '../pages/authpages/LoginPage';
 import RegisterPage from '../pages/authpages/RegisterPage';
+import PrivateRoute from './PrivateRoute';
 
 const router = createBrowserRouter([
   {
@@ -15,11 +16,19 @@ const router = createBrowserRouter([
     children: [
       {
         path: '/auth/login',
-        element: <LoginPage />,
+        element: (
+          <PrivateRoute>
+            <LoginPage />
+          </PrivateRoute>
+        ),
       },
       {
         path: '/auth/register',
-        element: <RegisterPage />,
+        element: (
+          <PrivateRoute>
+            <RegisterPage />
+          </PrivateRoute>
+        ),
       },
     ],
   },

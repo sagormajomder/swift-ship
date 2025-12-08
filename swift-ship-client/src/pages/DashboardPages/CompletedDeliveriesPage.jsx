58620747b5eb941af33b0b781ab79a63fab02789
Sagor Majomder <sagormajomder@gmail.com>
2025-12-08 23:48:55 +0600

    Add Completed Deliveries page with parcel data retrieval and payout calculation

--- swift-ship-client/src/pages/DashboardPages/CompletedDeliveriesPage.jsx
+++ swift-ship-client/src/pages/DashboardPages/CompletedDeliveriesPage.jsx
@@ -1,3 +1,66 @@
+import { useQuery } from '@tanstack/react-query';
+import { useAuth } from '../../contexts/AuthContext';
+import useAxiosSecure from '../../hooks/useAxiosSecure';
+
 export default function CompletedDeliveriesPage() {
-  return <div>CompletedDeliveriesPage</div>;
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+
+  const { data: parcels = [] } = useQuery({
+    queryKey: ['parcels', user.email, 'driver_assigned'],
+    queryFn: async () => {
+      const res = await axiosSecure.get(
+        `/parcels/rider?riderEmail=${user.email}&deliveryStatus=parcel_delivered`
+      );
+
+      return res.data;
+    },
+  });
+
+  const calculatePayout = parcel => {
+    if (parcel.senderDistrict === parcel.receiverDistrict) {
+      return parcel.cost * 0.8;
+    } else {
+      return parcel.cost * 0.6;
+    }
+  };
+
+  return (
+    <div>
+      <h2 className='text-4xl'>Completed Deliveries: {parcels.length}</h2>
+      <div className='overflow-x-auto'>
+        <table className='table table-zebra'>
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Created At</th>
+              <th>Pickup District</th>
+              <th>Cost</th>
+              <th>Payout</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {parcels.map((parcel, index) => (
+              <tr key={parcel._id}>
+                <th>{index + 1}</th>
+                <td>{parcel.parcelName}</td>
+                <td>{parcel.createdAt}</td>
+                <td>{parcel.senderDistrict}</td>
+                <td>{parcel.cost}</td>
+                <td>{calculatePayout(parcel)}</td>
+                <td>
+                  <button className='btn btn-primary text-black'>
+                    Cash out
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
